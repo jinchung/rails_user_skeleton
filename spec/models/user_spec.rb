ad06f38dee9a66d71c1e0a64807aabec9120b332
Jin Chung <hyojin.chung17@gmail.com>
2013-02-13 18:26:33 -0500

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -74,6 +74,16 @@ describe User do
     it { should_not be_valid }
   end
   
+  describe "email address with mixed case" do
+     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+  
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
@@ -94,7 +104,7 @@ describe User do
     let(:found_user) { User.find_by_email(@user.email) }
 
     describe "with valid password" do
-      it { should == found_user.authenticate(@user.password) }
+      it { should == found_user.authenticate(@user.password) }  
     end
 
     describe "with invalid password" do
